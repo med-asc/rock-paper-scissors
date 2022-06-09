5719ba61c01374ae6f3d3a61c34ec5bc64814761
Cristian Medina <medinacristian@hotmail.com>
2022-06-09 21:54:27 +0200

    Change the get player input func to arrow func

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-shape = ["Rock", "Paper", "Scissor"];
+shape = ["rock", "paper", "scissor"];
 
 // Set the scores to 0
 let player = 0;
@@ -6,11 +6,7 @@ let computer = 0;
 
 let computerPlay = () => Math.floor(Math.random() * shape.length);
 
-function getUserChoice(choice) {
-  // Fix player format by capitalizing first letter
-  choice = choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase();
-  return shape.indexOf(choice);
-}
+let getPlayerChoice = (choice) => shape.indexOf(choice);
 
 function playRound(playerSelection, computerSelection) {
   playerShape = shape[playerSelection];
@@ -38,7 +34,6 @@ function updateScore(str) {
     computer++;
   }
   return [player, computer]
-//   console.log(`Player score: ${player}, Computer score: ${computer}`);
 }
 
 const buttons = document.querySelectorAll(".btn");
@@ -48,7 +43,7 @@ const cScore = document.querySelector("#computer-score");
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    const playerChoice = getUserChoice(button.id);
+    const playerChoice = getPlayerChoice(button.id);
     const game = playRound(playerChoice, computerPlay());
     let score = updateScore(game);
     pScore.textContent = score[0]
