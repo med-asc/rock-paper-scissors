ad7a6116cdf25e6cc9c6fd3c3ca601a850c226be
Cristian Medina <medinacristian@hotmail.com>
2022-06-09 17:33:05 +0200

    Add eventListner to btns that calls playRound()

--- script.js
+++ script.js
@@ -1,50 +1,57 @@
-shape = ['Rock', 'Paper', 'Scissor']
+shape = ["Rock", "Paper", "Scissor"];
 
 let computerPlay = () => Math.floor(Math.random() * shape.length);
 
-function getUserChoice() {
-    player = prompt('Rock, Paper or Scissor?')
-    // Fix player format by capitalizing first letter
-    player = player.charAt(0).toUpperCase() + player.slice(1).toLowerCase()
-    return shape.indexOf(player)
+function getUserChoice(choice) {
+  // Fix player format by capitalizing first letter
+  choice = choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase();
+  return shape.indexOf(choice);
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerShape = shape[playerSelection];
-    computerShape = shape[computerSelection];
+  playerShape = shape[playerSelection];
+  computerShape = shape[computerSelection];
 
-    if (playerSelection == 0 && computerSelection == 2) {
-        output = `You Win! ${playerShape} beats ${computerShape}`;
-    }
-    else if ((playerSelection - computerSelection) == 1) {
-        output = `You Win! ${playerShape} beats ${computerShape}`;
-    } else if (playerSelection == computerSelection) {
-        output = `Draw! ${playerShape} and ${computerShape}`;
-    } else {
-        output = `You Lose! ${computerShape} beats ${playerShape}`;
-    }
-    return output
+  if (playerSelection == 0 && computerSelection == 2) {
+    output = `You Win! ${playerShape} beats ${computerShape}`;
+  } else if (playerSelection - computerSelection == 1) {
+    output = `You Win! ${playerShape} beats ${computerShape}`;
+  } else if (playerSelection == computerSelection) {
+    output = `Draw! ${playerShape} and ${computerShape}`;
+  } else {
+    output = `You Lose! ${computerShape} beats ${playerShape}`;
+  }
+  return output;
 }
 
 function game() {
-    // Set the scores to 0
-    let player = 0
-    let computer = 0
+  // Set the scores to 0
+  let player = 0;
+  let computer = 0;
 
-    const playerSelection = getUserChoice();
-    const computerSelection = computerPlay();
+  const playerSelection = getUserChoice();
+  const computerSelection = computerPlay();
 
-    let round = playRound(playerSelection, computerSelection)
-    
-    if (round.charAt(4) == 'W') {
-        player++
-    } else if (round.charAt(4) == 'L') {
-        computer++
-    }
-    else {
-        player++
-        computer++
-    }
-    console.log(round)
-    console.log(`Player score: ${player}, Computer score: ${computer}`)
+  let round = playRound(playerSelection, computerSelection);
+
+  if (round.charAt(4) == "W") {
+    player++;
+  } else if (round.charAt(4) == "L") {
+    computer++;
+  } else {
+    player++;
+    computer++;
+  }
+  console.log(round);
+  console.log(`Player score: ${player}, Computer score: ${computer}`);
 }
+
+const buttons = document.querySelectorAll(".btn");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const playerChoice = getUserChoice(button.id);
+    const game = playRound(playerChoice, computerPlay());
+    console.log(game);
+  });
+});
